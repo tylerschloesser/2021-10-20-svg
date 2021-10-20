613cbe5462d701ef822d773b169317e7b30840b1
Tyler Schloesser <tschloes@amazon.com>
2021-10-20 07:25:28 -0700

    unanimate

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// <animate
+//   calcMode="paced"
+//   attributeName="font-size"
+//   values="80px;100px;80px"
+//   dur="5s"
+//   repeatCount="indefinite"
+// />
+
 const App = () => (
   <svg>
     <text color="black" fontSize="80px" dominantBaseline="hanging">
